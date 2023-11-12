91047d55f7f49f5884995fc52e572ddb7cc28147
brandonTsai <brandonya125@gmail.com>
2023-11-11 21:47:18 -0800

    Define base addresses of memories

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
@@ -1,6 +1,17 @@
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
+/* 
+ * base address of Flash and SRAM memories
+ */
+
+#define FLASH_BASEADDR      0x08000000U         /* Base address of Flash        */
+#define SRAM1_BASEADDR      0x20000000U         /* Base address of SRAM1        */
+#define SRAM2_BASEADDR      0x20001C00U         /* Base address of SRAM2        */
+#define ROM_BASEADDR        0x1FFF0000U         /* Base address of ROM          */
+#define SRAM_BASEADDR       SRAM1_BASEADDR      /* Base address of Main SRAM    */
+
+
 
 
 #endif /* INC_STM32F407XX_H_ */
