c75308051c5de36268b959df7496512dd3d05d9f
brandonTsai <brandonya125@gmail.com>
2023-11-13 06:43:41 -0800

    Define base address for peripherals on APB1 bus

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
@@ -28,15 +28,31 @@
  * TODO : Complete for all other peripherals
  */
 
-#define GPIOA_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0000U)
-#define GPIOB_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0400U)
-#define GPIOC_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0800U)
-#define GPIOD_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0C00U)
-#define GPIOE_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1000U)
-#define GPIOF_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1400U)
-#define GPIOG_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1800U)
-#define GPIOH_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1C00U)
-#define GPIOI_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x2000U)
+#define GPIOA_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0000)
+#define GPIOB_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0400)
+#define GPIOC_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0800)
+#define GPIOD_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0C00)
+#define GPIOE_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1000)
+#define GPIOF_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1400)
+#define GPIOG_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1800)
+#define GPIOH_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1C00)
+#define GPIOI_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x2000)
+
+
+/* 
+ * base address of peripherals which hanging on APB1 bus
+ * TODO : Complete for all other peripherals
+ */
+
+#define I2C1_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x5400)
+#define I2C2_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x5800)
+#define I2C3_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x5C00)
+#define SPI2_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x3800)
+#define SPI3_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x3C00)
+#define UART2_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x4400)
+#define UART3_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x4800)
+#define UART4_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x4C00)
+#define UART5_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x5000)
 
 
 #endif /* INC_STM32F407XX_H_ */
