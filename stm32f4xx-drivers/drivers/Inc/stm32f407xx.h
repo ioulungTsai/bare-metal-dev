7fc7c4964d69ee548d95d8cfd4e7cc7a3f9da4fb
brandonTsai <brandonya125@gmail.com>
2023-11-13 06:53:34 -0800

    Correct naming on USARTx

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
@@ -46,8 +46,8 @@
 
 #define SPI2_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x3800)
 #define SPI3_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x3C00)
-#define UART2_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x4400)
-#define UART3_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x4800)
+#define USART2_BASEADDR                 ((APB1PERIPH_BASEADDR) + 0x4400)
+#define USART3_BASEADDR                 ((APB1PERIPH_BASEADDR) + 0x4800)
 #define UART4_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x4C00)
 #define UART5_BASEADDR                  ((APB1PERIPH_BASEADDR) + 0x5000)
 #define I2C1_BASEADDR                   ((APB1PERIPH_BASEADDR) + 0x5400)
@@ -60,8 +60,8 @@
  * TODO : Complete for all other peripherals
  */
 
-#define UART1_BASEADDR                  ((APB2PERIPH_BASEADDR) + 0x1000)
-#define UART6_BASEADDR                  ((APB2PERIPH_BASEADDR) + 0x1400)
+#define USART1_BASEADDR                 ((APB2PERIPH_BASEADDR) + 0x1000)
+#define USART6_BASEADDR                 ((APB2PERIPH_BASEADDR) + 0x1400)
 #define SPI1_BASEADDR                   ((APB2PERIPH_BASEADDR) + 0x3000)
 #define SYSCFG_BASEADDR                 ((APB2PERIPH_BASEADDR) + 0x3800)
 #define EXTI_BASEADDR                   ((APB2PERIPH_BASEADDR) + 0x3C00)
