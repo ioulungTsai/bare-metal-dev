a071e0053795cd3bee51eafdbd429c0a82e19a6c
brandonTsai <brandonya125@gmail.com>
2023-11-13 06:35:24 -0800

    Define base address of GPIOs on AHB1 bus

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
@@ -13,7 +13,7 @@
 
 
 /* 
- * base address of Flash and SRAM memories
+ * base address of AHBx and APBx buses
  */
 
 #define PERIPH_BASEADDR                 0x40000000U
@@ -21,6 +21,22 @@
 #define APB2PERIPH_BASEADDR             0x40010000U
 #define AHB1PERIPH_BASEADDR             0x40020000U
 #define AHB2PERIPH_BASEADDR             0x50000000U
+
+
+/* 
+ * base address of peripherals which hanging on AHB1 bus
+ * TODO : Complete for all other peripherals
+ */
+
+#define GPIOA_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0000U)
+#define GPIOB_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0400U)
+#define GPIOC_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0800U)
+#define GPIOD_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x0C00U)
+#define GPIOE_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1000U)
+#define GPIOF_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1400U)
+#define GPIOG_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1800U)
+#define GPIOH_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x1C00U)
+#define GPIOI_BASEADDR                  ((AHB1PERIPH_BASEADDR) + 0x2000U)
 
 
 #endif /* INC_STM32F407XX_H_ */
