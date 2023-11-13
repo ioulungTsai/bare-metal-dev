a62a878819d37418637c8bea2fb50c793f4c1c92
brandonTsai <brandonya125@gmail.com>
2023-11-13 06:05:16 -0800

    Define base address of bus domains

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
@@ -5,13 +5,22 @@
  * base address of Flash and SRAM memories
  */
 
-#define FLASH_BASEADDR      0x08000000U         /* Base address of Flash        */
-#define SRAM1_BASEADDR      0x20000000U         /* Base address of SRAM1        */
-#define SRAM2_BASEADDR      0x20001C00U         /* Base address of SRAM2        */
-#define ROM_BASEADDR        0x1FFF0000U         /* Base address of ROM          */
-#define SRAM_BASEADDR       SRAM1_BASEADDR      /* Base address of Main SRAM    */
+#define FLASH_BASEADDR                  0x08000000U         /* Base address of Flash        */
+#define SRAM1_BASEADDR                  0x20000000U         /* Base address of SRAM1        */
+#define SRAM2_BASEADDR                  0x20001C00U         /* Base address of SRAM2        */
+#define ROM_BASEADDR                    0x1FFF0000U         /* Base address of ROM          */
+#define SRAM_BASEADDR                   SRAM1_BASEADDR      /* Base address of Main SRAM    */
 
 
+/* 
+ * base address of Flash and SRAM memories
+ */
+
+#define PERIPH_BASEADDR                 0x40000000U
+#define APB1PERIPH_BASEADDR             PERIPH_BASEADDR
+#define APB2PERIPH_BASEADDR             0x40010000U
+#define AHB1PERIPH_BASEADDR             0x40020000U
+#define AHB2PERIPH_BASEADDR             0x50000000U
 
 
 #endif /* INC_STM32F407XX_H_ */
