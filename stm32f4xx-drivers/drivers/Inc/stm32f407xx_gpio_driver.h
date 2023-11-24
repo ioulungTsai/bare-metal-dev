02183b416e003f6c62bba74cb4b3963fa9e578dc
brandonTsai <brandonya125@gmail.com>
2023-11-23 23:36:23 -0800

    Defining GPIO handle and configuration structure

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx_gpio_driver.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx_gpio_driver.h
@@ -4,5 +4,34 @@
 
 #include "stm32f407xx.h"
 
+/*
+ * This is a Configuration structure for a GPIO pin   
+ */
+
+typedef struct
+{
+    uint8_t GPIO_PinNumber;
+    uint8_t GPIO_PinMode;
+    uint8_t GPIO_PinSpeed;
+    uint8_t GPIO_PinPuPdControl;
+    uint8_t GPIO_PinOPType;
+    uint8_t GPIO_PinAltFunMode;
+
+}GPIO_Pinconfig_t;
+
+
+
+/*
+ * This is a Handle structure for a GPIO pin 
+ */
+
+typedef struct
+{
+    // pointer to hold the base address of the GPIO peripheral
+    GPIO_RegDef_t *pGPIOx;                  /*!< This holds the base address of the GPIO port to which the pin belongs >*/
+    GPIO_Pinconfig_t GPIO_Pinconfig;        /*!< This holds GPIO pin configuration settings >*/
+
+}GPIO_Handle_t;
+
 
 #endif /* INC_STM32F407XX_GPIO_DRIVER_H_ */
