c9ab27b2a3c11d2f6a416559fed30376237f1f4f
brandonTsai <brandonya125@gmail.com>
2023-11-28 06:59:38 -0800

    Initiate empty driver api and documentation

--- stm32f4xx-drivers/drivers/Src/stm32f407xx_gpio_driver.c
+++ stm32f4xx-drivers/drivers/Src/stm32f407xx_gpio_driver.c
@@ -1,2 +1,83 @@
 # include "stm32f407xx_gpio_driver.h"
 
+
+/*
+ * Peripheral Clock setup
+ */
+
+/*********************************************************************
+ * @fn      		  - GPIO_PeriClockControl
+ *
+ * @brief             - This function enables or disables peripheral clock for the given GPIO port
+ *
+ * @param[in]         - Base address of the GPIO peripheral
+ * @param[in]         - ENABLE or DISABLE macros
+ * @param[in]         -
+ *
+ * @return            -  none
+ *
+ * @Note              -  none
+
+ ********************************************************************/
+
+void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
+{
+
+}
+
+/*
+ * Init and De-init
+ */
+
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
+{
+
+}
+
+void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
+{
+
+}
+
+/*
+ * Data read and write
+ */
+
+uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
+{
+
+}
+
+uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
+{
+
+}
+
+void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value)
+{
+
+}
+
+void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value)
+{
+
+}
+
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
+{
+
+}
+
+/*
+ * IRQ Configuration and ISR handling
+ */
+
+void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi)
+{
+
+}
+
+void GPIO_IRQHandling(uint8_t PinNumber)
+{
+
+}
