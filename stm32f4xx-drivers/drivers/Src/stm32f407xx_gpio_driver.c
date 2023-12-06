29c569b1a69cd146c137736eb9476d23ddbaeb7a
brandonTsai <brandonya125@gmail.com>
2023-12-05 21:44:50 -0800

    Implement GPIO peripheral clock control API

--- stm32f4xx-drivers/drivers/Src/stm32f407xx_gpio_driver.c
+++ stm32f4xx-drivers/drivers/Src/stm32f407xx_gpio_driver.c
@@ -1,9 +1,4 @@
-# include "stm32f407xx_gpio_driver.h"
-
-
-/*
- * Peripheral Clock setup
- */
+#include "stm32f407xx_gpio_driver.h"
 
 /*********************************************************************
  * @fn      		  - GPIO_PeriClockControl
@@ -22,7 +17,84 @@
 
 void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
 {
-
+    if (EnorDi == ENABLE)
+    {
+        if (pGPIOx == GPIOA)
+        {
+            GPIOA_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOB)
+        {
+            GPIOB_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOC)
+        {
+            GPIOC_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOD)
+        {
+            GPIOD_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOE)
+        {
+            GPIOE_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOF)
+        {
+            GPIOF_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOG)
+        {
+            GPIOG_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOH)
+        {
+            GPIOH_PCLK_EN();
+        }
+        else if (pGPIOx == GPIOI)
+        {
+            GPIOI_PCLK_EN();
+        }
+    }
+    else
+    {
+         if (pGPIOx == GPIOA)
+        {
+            GPIOA_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOB)
+        {
+            GPIOB_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOC)
+        {
+            GPIOC_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOD)
+        {
+            GPIOD_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOE)
+        {
+            GPIOE_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOF)
+        {
+            GPIOF_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOG)
+        {
+            GPIOG_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOH)
+        {
+            GPIOH_PCLK_DI();
+        }
+        else if (pGPIOx == GPIOI)
+        {
+            GPIOI_PCLK_DI();
+        }
+    }
 }
 
 /*
@@ -31,12 +103,10 @@ void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx, uint8_t EnorDi)
 
 void GPIO_Init(GPIO_Handle_t *pGPIOHandle)
 {
-
 }
 
 void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
 {
-
 }
 
 /*
@@ -45,27 +115,22 @@ void GPIO_DeInit(GPIO_RegDef_t *pGPIOx)
 
 uint8_t GPIO_ReadFromInputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
 {
-
 }
 
 uint16_t GPIO_ReadFromInputPort(GPIO_RegDef_t *pGPIOx)
 {
-
 }
 
 void GPIO_WriteToOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber, uint8_t Value)
 {
-
 }
 
 void GPIO_WriteToOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t Value)
 {
-
 }
 
 void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
 {
-
 }
 
 /*
@@ -74,10 +139,8 @@ void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t PinNumber)
 
 void GPIO_IRQConfig(uint8_t IRQNumber, uint8_t IRQPriority, uint8_t EnorDi)
 {
-
 }
 
 void GPIO_IRQHandling(uint8_t PinNumber)
 {
-
 }
