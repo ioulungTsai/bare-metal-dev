c56cf23f38bdd407d4a7b81ebbbb9e24ca3a94f0
brandonTsai <brandonya125@gmail.com>
2023-12-28 01:50:10 -0800

    Define SYSCFG structure

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx.h
@@ -160,6 +160,21 @@ typedef struct
 
 
 /*
+ * Peripheral register definition structure for SYSCFG
+ */
+
+typedef struct
+{
+    __vo uint32_t MEMRMP;                    /* SYSCFG memory remap register                             Address offset: 0x00          */
+    __vo uint32_t PMC;                       /* SYSCFG peripheral mode configuration register            Address offset: 0x04          */
+    __vo uint32_t EXTICR[4];                 /* SYSCFG external interrupt configuration register 1-4     Address offset: 0x08-0x14     */
+         uint32_t RESERVED[2];               /* Reserved                                                 Address offset: 0x18-0x1C     */
+    __vo uint32_t CMPCR;                     /* Compensation cell control register                       Address offset: 0x20          */                  Address offset: 0x14 */
+
+}SYSCFG_RegDef_t;
+
+
+/*
  * Peripheral definitions (Peripheral base addresses typecasted to xxx_RegDef_t) 
  */
 
