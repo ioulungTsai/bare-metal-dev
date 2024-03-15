214aa8d938d4e468dd466734cc5194f107ad5ad4
brandonTsai <brandonya125@gmail.com>
2024-03-15 09:04:07 -0700

    Add SPI driver API prototype

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx_spi_driver.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx_spi_driver.h
@@ -39,4 +39,50 @@ typedef struct
 }SPI_Handle_t;
 
 
+/*****************************************************************************
+ *                      APIs supported by this driver
+ *      For more information about the APIs check the function definitions
+******************************************************************************/
+
+/*
+ * Peripheral Clock setup
+ */
+
+void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t EnorDi);
+
+/*
+ * Init and De-init
+ */
+
+void SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+
+/*
+ * Data Send and Receive
+ */
+
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t Len);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t Len);
+
+
+/*
+ * IRQ Configuration and ISR handling
+ */
+
+void SPI_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDi);
+void SPI_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority);
+void SPI_IRQHandling(SPI_Handle_t *pSPIHandle);
+
+
+/*
+ * Other Peripheral Control APIs
+ */
+
+
+/*
+ * Application callback
+ */
+
+
 #endif /* INC_STM32F407XX_SPI_DRIVER_H_ */
