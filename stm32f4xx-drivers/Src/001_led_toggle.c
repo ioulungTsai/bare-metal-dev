89dc954cdbd7f8e937e74ac9ffae8d2adff6c540
brandonTsai <brandonya125@gmail.com>
2024-03-18 06:41:42 -0700

    Change GPIO pin output type to push pull

--- stm32f4xx-drivers/Src/001_led_toggle.c
+++ stm32f4xx-drivers/Src/001_led_toggle.c
@@ -14,8 +14,8 @@ int main(void)
     GpioLed.GPIO_Pinconfig.GPIO_PinNumber = GPIO_PIN_NO_12;
     GpioLed.GPIO_Pinconfig.GPIO_PinMode = GPIO_MODE_OUT;
     GpioLed.GPIO_Pinconfig.GPIO_PinOPSpeed = GPIO_OUT_SPEED_FAST;
-    GpioLed.GPIO_Pinconfig.GPIO_PinOPType = GPIO_OUT_TYPE_OD;
-    // GpioLed.GPIO_Pinconfig.GPIO_PinOPType = GPIO_OUT_TYPE_PP;
+    // GpioLed.GPIO_Pinconfig.GPIO_PinOPType = GPIO_OUT_TYPE_OD;
+    GpioLed.GPIO_Pinconfig.GPIO_PinOPType = GPIO_OUT_TYPE_PP;
     GpioLed.GPIO_Pinconfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 
     GPIO_PeriClockControl(GPIOD, ENABLE);
