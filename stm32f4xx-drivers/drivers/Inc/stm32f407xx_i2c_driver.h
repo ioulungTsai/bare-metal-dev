19794b2a2d44e2d0377b71b508cc7a4480b011fe
brandonTsai <brandonya125@gmail.com>
2024-05-06 07:24:36 -0700

    Add I2C user configurable macros

--- stm32f4xx-drivers/drivers/Inc/stm32f407xx_i2c_driver.h
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx_i2c_driver.h
@@ -13,6 +13,7 @@
 /*
  * Configuration structure for I2Cx peripheral
  */
+
 typedef struct
 {
 	uint32_t I2C_SCLSpeed;
@@ -23,14 +24,40 @@ typedef struct
 }I2C_Config_t;
 
 /*
- *Handle structure for I2Cx peripheral
+ * Handle structure for I2Cx peripheral
  */
+
 typedef struct
 {
 	I2C_RegDef_t 	*pI2Cx;
 	I2C_Config_t 	I2C_Config;
+
 }I2C_Handle_t;
 
+
+/*
+ * @I2C_SCLSpeed
+ */
+
+#define I2C_SCL_SPEED_SM 	100000
+#define I2C_SCL_SPEED_FM4K 	400000
+#define I2C_SCL_SPEED_FM2K  200000
+
+
+/*
+ * @I2C_AckControl
+ */
+
+#define I2C_ACK_ENABLE        1
+#define I2C_ACK_DISABLE       0
+
+
+/*
+ * @I2C_FMDutyCycle
+ */
+
+#define I2C_FM_DUTY_2        0
+#define I2C_FM_DUTY_16_9     1
 
 
 #endif
