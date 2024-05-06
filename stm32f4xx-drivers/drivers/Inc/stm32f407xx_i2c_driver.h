ee9f0997411a6579a9f3d2306e2839332bdd4d4b
brandonTsai <brandonya125@gmail.com>
2024-05-05 23:03:57 -0700

    Create header and source files for I2C driver

---
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx_i2c_driver.h
@@ -0,0 +1,36 @@
+/*
+ * stm32f407xx_i2c_driver.h
+ *
+ *  Created on: May 5 2024
+ *      Author: Brandon Tsai
+ */
+
+#ifndef INC_STM32F407XX_I2C_DRIVER_H_
+#define INC_STM32F407XX_I2C_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+/*
+ * Configuration structure for I2Cx peripheral
+ */
+typedef struct
+{
+	uint32_t I2C_SCLSpeed;
+	uint8_t  I2C_DeviceAddress;
+	uint8_t  I2C_AckControl;
+	uint8_t  I2C_FMDutyCycle;
+
+}I2C_Config_t;
+
+/*
+ *Handle structure for I2Cx peripheral
+ */
+typedef struct
+{
+	I2C_RegDef_t 	*pI2Cx;
+	I2C_Config_t 	I2C_Config;
+}I2C_Handle_t;
+
+
+
+#endif
---
+++ stm32f4xx-drivers/drivers/Src/stm32f407xx_i2c_driver.c
@@ -0,0 +1,9 @@
+/*
+ * stm32f407xx_i2c_driver.c
+ *
+ *  Created on: May 5 2024
+ *      Author: admin
+ */
+
+#include "stm32f407xx_i2c_driver.h"
+
