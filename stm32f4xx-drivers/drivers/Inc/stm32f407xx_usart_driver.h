16021f4b375a81620b4c76fe7c60be2706f14dc8
Brandon Tsai <brandonya125@gmail.com>
2024-05-13 12:48:35 -0700

    Create USART header file and add configuration and handle structures

---
+++ stm32f4xx-drivers/drivers/Inc/stm32f407xx_usart_driver.h
@@ -0,0 +1,46 @@
+/*
+ * stm32f407xx_usart_driver.h
+ *
+ *  Created on: May 13 2024
+ *      Author: Brandon Tsai
+ */
+
+#ifndef INC_STM32F407XX_USART_DRIVER_H_
+#define INC_STM32F407XX_USART_DRIVER_H_
+
+#include "stm32f407xx.h"
+
+
+/*
+ * Configuration structure for USARTx peripheral
+ */
+
+typedef struct
+{
+	uint8_t USART_Mode;
+	uint32_t USART_Baud;
+	uint8_t USART_NoOfStopBits;
+	uint8_t USART_WordLength;
+	uint8_t USART_ParityControl;
+	uint8_t USART_HWFlowControl;
+
+}USART_Config_t;
+
+
+/*
+ * Handle structure for USARTx peripheral
+ */
+
+typedef struct
+{
+	USART_RegDef_t *pUSARTx;
+	USART_Config_t USART_Config;
+
+}USART_Handle_t;
+
+
+
+
+
+
+#endif
