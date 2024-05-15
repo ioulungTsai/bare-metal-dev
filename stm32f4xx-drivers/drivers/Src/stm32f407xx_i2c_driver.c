c2fc4ee35bc487d4113ca834847bf6b58c0ab730
Brandon Tsai <brandonya125@gmail.com>
2024-05-15 10:50:00 -0700

    Create the RCC driver header and source files to separate the corresponding APIs from other driver files

--- stm32f4xx-drivers/drivers/Src/stm32f407xx_i2c_driver.c
+++ stm32f4xx-drivers/drivers/Src/stm32f407xx_i2c_driver.c
@@ -7,8 +7,6 @@
 
 #include "stm32f407xx_i2c_driver.h"
 
-uint16_t AHB_Prescaler[8] = {2, 4, 8, 16, 64, 128, 256, 512};
-uint8_t APB1_Prescaler[4] = {2, 4, 8, 16};
 
 static void I2C_GenerateStartCondition(I2C_RegDef_t *pI2Cx);
 static void I2C_ExecuteAddressPhaseWrite(I2C_RegDef_t *pI2Cx, uint8_t SlaveAddr);
@@ -140,52 +138,6 @@ void I2C_PeriClockControl(I2C_RegDef_t *pI2Cx, uint8_t EnorDi)
         }
     }
 }
-
-
-uint32_t  RCC_GetPLLOutputClock()
-{
-	return 0;
-}
-
-uint32_t RCC_GetPCLK1Value(void)
-{
-    uint32_t pclk1, SystemClk;
-
-    uint8_t clksrc, temp, ahbp, abp1p;
-
-    clksrc = ((RCC->CFGR >> 2) & 0x3);
-
-    if(clksrc == 0) {
-        SystemClk = 16000000;
-    } else if (clksrc == 1) {
-        SystemClk = 8000000;
-    } else if (clksrc == 2) {
-        SystemClk = RCC_GetPLLOutputClock();
-    }
-
-    // AHB
-    temp = ((RCC->CFGR >> 4) & 0xF);
-
-    if(temp < 8) {
-        ahbp = 1;
-    } else {
-        ahbp = AHB_Prescaler[temp - 8];
-    }
-
-    // APB1
-    temp = ((RCC->CFGR >> 10) & 0x7);
-
-    if(temp < 4) {
-        abp1p = 1;
-    } else {
-        abp1p = APB1_Prescaler[temp - 4];
-    }
-
-    pclk1 = (SystemClk / ahbp) / abp1p;
-
-    return pclk1;
-}
-
 
 
 /*
